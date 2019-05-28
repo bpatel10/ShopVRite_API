b50c9a3538615bb0242faf670584ef83886f6bc7
Binita Patel <binita.patel@wakefern.com>
2019-05-28 09:58:01 -0400

    add data

--- shopvrite/data/datajson.js
+++ shopvrite/data/datajson.js
@@ -1,13 +1,25 @@
 exports.QRCodes=[
     {
         QRCode:'505050',
-        points:120
+        points:25
     }
 ];
 exports.customer=[
     {
         fsn:'48501299232',
         name:'Binita Patel'
+    },
+    {
+        fsn:'485017749386',
+        name:'Abhi Ray'
+    },
+    {
+        fsn:'471096444304',
+        name:'Abhinav Ray'
+    },
+    {
+        fsn:'485018656430',
+        name:'App Tester'
     }
 ];
 exports.customerScanned={};/*{
